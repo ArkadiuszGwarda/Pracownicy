160b52b1b55b67424dc0fd8398579559eac23a1d
PiotrMatenka <piotrmatenka@gmail.com>
2018-12-29 15:29:36 +0100

    Utworzono klasę Person i poprawiono Address

--- src/puw/model/Main.java
+++
@@ -1,9 +0,0 @@
-package puw.model;
-
-public class Main {
-    public static void main(String[] args) {
-
-        Person person = new Person("andrzej","dsdd", "900307453123", new Address("jakstaM", "14", "21-4444","jakiestam"));
-        System.out.println(person);
-    }
-}
