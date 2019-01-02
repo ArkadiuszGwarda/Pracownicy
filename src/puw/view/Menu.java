ad1c4b17010c06980045e079fb04252761a9baa3
PatrykJablonski <pjablonski0309@gmail.com>
2019-01-03 00:53:39 +0100

    Utworzono klasę Menu #107

---
+++ src/puw/view/Menu.java
@@ -0,0 +1,32 @@
+package puw.view;
+
+        import java.util.ArrayList;
+        import java.util.List;
+
+public class Menu {
+    private Menu parentMenu;
+    private List<MenuItem> items;
+
+    public Menu() {
+        this.parentMenu = null;
+        items = new ArrayList<>();
+    }
+    public Menu(Menu parentMenu) {
+        this();
+        this.parentMenu = parentMenu;
+    }
+
+    public void addItem(MenuItem item) {
+        items.add(item);
+    }
+
+    public MenuItem getItem(int index) {
+        return items.get(index);
+    }
+
+    public void display() {
+        for(int i = 0; i < items.size(); i++) {
+            System.out.println(i + 1 + ". " + items.get(i).getName());
+        }
+    }
+}
